fdc1e559562c67c16d25a6627d73954f6e707b9e
Võ Tiến Khoa <votienkhoa16vl@gmail.com>
2023-11-16 15:03:38 +0700

    inintal project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
-import Button from './pages/button';
-
 function App() {
   return (
     <div className='w-full max-h-full bg-white max'>
-        <Button />
     </div>
   );
 }
--- src/values/icon.js
+++
@@ -1,7 +0,0 @@
-import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
-
-const icons = {
-    AddShoppingCartIcon,
-}
-
-export default icons;
