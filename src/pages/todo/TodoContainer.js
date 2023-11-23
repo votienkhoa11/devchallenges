e417c9fc852bfeb3b949721ab6dab0c39695a226
Võ Tiến Khoa <votienkhoa16vl@gmail.com>
2023-11-23 13:08:49 +0700

    Add local storage services, save and load todos from local storage

--- src/pages/todo/TodoContainer.js
+++ src/pages/todo/TodoContainer.js
@@ -1,30 +1,47 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import TodomainView from './template/TodomainView'
 import { v4 as uuidv4 } from 'uuid';
+import { load, save } from '../../utils/LocalStorageServices';
+import localStorageKeys from '../../utils/LocalStorageKey';
 
 function TodoContainer() {
     const [todoList, setTodoList]= useState([]);;
 
     const createTodo = (todo) => {
-        setTodoList([
-            ...todoList,
-            {id: uuidv4(), task: todo, completed: false}
-        ]);
+        const newtodoList = [...todoList, {id: uuidv4(), task: todo, completed: false}]
+        setTodoList(newtodoList);
 
-        console.log(todoList)
+        const lstResult = save (localStorageKeys.todos, newtodoList)
+        if (!lstResult) {
+            console.log(lstResult);
+        }
+      
     }
 
     const deleteTodo = (id) => {
-        setTodoList(todoList.filter((todo) => todo.id !== id))
+        const newtodoList = todoList.filter(todo => todo.id !== id);
+        setTodoList(newtodoList);
+        
+        const lstResult = save(localStorageKeys.todos, newtodoList)
+        if (!lstResult) {
+            console.log(lstResult);
+        }
     };
 
     const checkComplete = (id) => {
-        setTodoList(
-            todoList.map((todo) =>
-                todo.id === id ? {...todo, completed: !todo.completed} : todo,
-            )
-        );
+        const newtodoList = todoList.map(todo => todo.id === id ? {...todo, completed: !todo.completed} : todo)
+        setTodoList(newtodoList);
+        
+        const lstResult = save(localStorageKeys.todos, newtodoList)
+        if (!lstResult) {
+            console.log(lstResult);
+        }
     }
+
+    useEffect(() => {
+        const savedTodos = load(localStorageKeys.todos) || [];
+        setTodoList(savedTodos);
+    }, [])
 
     const todoProps = {
         //values
--- src/pages/todo/template/subView/Todo.js
+++ src/pages/todo/template/subView/Todo.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import icons from '../../../../values/icon'
-import { CheckBox } from '@mui/icons-material'
 
 function Todo({todo, deleteTodo, checkComplete}) {
   return (
         <div className='flex w-[608px] items-center flex-row space-x-[7px]'>
-            <CheckBox
-                defaultChecked
+            <input
+                type='checkbox'
+                defaultChecked={todo.completed}
+                onChange={() => checkComplete(todo.id)}
             />
             <p className='w-full text-black text-[18px] font-medium pb-[1px]'>{todo.task}</p>
             <div onClick={() => deleteTodo(todo.id)} className='pb-[2px]'>
