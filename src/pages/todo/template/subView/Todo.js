b02564f44384dbd50b2da6fdd748acc5c9995cbf
Võ Tiến Khoa <votienkhoa16vl@gmail.com>
2023-11-27 12:47:30 +0700

    fix bug text overflow

--- src/pages/todo/template/subView/Todo.js
+++ src/pages/todo/template/subView/Todo.js
@@ -4,15 +4,16 @@ import icons from '../../../../values/icon'
 function Todo({todo, deleteTodo, checkComplete}) {
     let line = todo.completed && ' line-through';
   return (
-        <div className='flex w-[608px] items-center flex-row space-x-[7px]'>
+        <div className='flex w-[608px] items-center flex-row space-x-[8px]'>
             <input
                 type='checkbox'
                 defaultChecked={todo.completed}
                 onChange={() => checkComplete(todo.id)}
+                className='h-[24px] w-[24px] accent-blue'
             />
             <p 
                 className={
-                    'w-full text-black text-[18px] font-medium pb-[1px]' + line
+                    'w-full text-black text-[18px] font-medium pb-[1px] flex-wrap break-all text-clip' + line
                 }
             >
                 {todo.task}
